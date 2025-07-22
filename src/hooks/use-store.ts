544b7a66960225950d2268d4ed620bb0da0df99c
nocodeci <yohankoffik@gmail.com>
2025-07-22 17:49:49 +0000

    v10
    nn

---
+++ src/hooks/use-store.ts
@@ -0,0 +1,14 @@
+"use client"
+
+import { useContext } from "react"
+import { StoreContext } from "@/context/store-context"
+
+export const useStore = () => {
+  const context = useContext(StoreContext)
+
+  if (!context) {
+    throw new Error("useStore must be used within a StoreProvider")
+  }
+
+  return context
+}
